9f8f2feecb9eecfb9a4de065d25b9fa626cd2e0a
Xavier St-Georges <xavierst-georges@hotmail.com>
2016-01-23 15:42:15 -0500

    swtch + ui modif

--- Cheese_v.0.2/Assets/Scripts/UI.cs
+++ Cheese_v.0.2/Assets/Scripts/UI.cs
@@ -3,29 +3,24 @@ using System.Collections;
 using UnityEngine.UI; 
 
 public class UI : MonoBehaviour {
-	//public Text timetext = Gameobject.Find("Text").getComponent<Text>();
 	public Text timetext;
-	public RawImage enveloppeuse;
-	public RawImage encaisseuse;
 	private int min;
 	private int seconds;
-	private int fraction;
 	private float starttime;
 	private float time;
 	public Text objectivetruck;
 	public int cheesestatus;
-	public Texture2D envelop;
+	public Texture texturemachine;
 	// Use this for initialization
 	void Start () {
-		starttime = Time.time;
 		min = 2;
 		seconds = 0;
-		timetext.rectTransform.position.Set (32, 133, 0);
+		timetext.transform.position.Set (32, 133, 0);
 		timetext.text = "Next Truck :" + min.ToString () + ":" + seconds.ToString ();
 		objectivetruck.text = cheesestatus.ToString () + "/10";
-	
 
 
+		
 	}
 	
 	// Update is called once per frame
@@ -37,13 +32,14 @@ public class UI : MonoBehaviour {
 			else {
 				seconds = 59;
 				min -= 1;
-
+				
 			}
-
+			
 			time -= 1;
 		}
-		timetext.rectTransform.position.Set (32, 133, 0);
+		timetext.transform.position.Set (32, 133, 0);
 		timetext.text = "Next Truck :" + min.ToString () + ":" + seconds.ToString ();
 
+		
 	}
 }
---
+++ Cheese_v.0.2/Assets/Scripts/swtch.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class swtch : MonoBehaviour {
+	
+	public GameObject ImageOnPanel;  ///set this in the inspector
+	public Texture NewTexture;
+	private RawImage img;
+	
+	void Start () {
+		img = (RawImage)ImageOnPanel.GetComponent<RawImage>();
+		
+
+	}
+	
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.U)) {
+			img.texture = (Texture)NewTexture;
+		}
+
+
+
+	}
+}
