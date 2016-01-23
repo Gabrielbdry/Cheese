7a6e824f7ad02f8c90980902e55515b6fd1733ff
Nicolas Coutu <nico.coutu@hotmail.com>
2016-01-23 10:49:38 -0500

    manager spaw

--- Cheese_v.0.2/Assets/MobSpawner.cs
+++ Cheese_v.0.2/Assets/MobSpawner.cs
@@ -3,11 +3,32 @@ using System.Collections;
 
 public class MobSpawner : MonoBehaviour {
     
+	private float lastTime;
+	public float frequency;
+	public Rigidbody _mob;
+
 	void Start () {
-	
+		lastTime = Time.time;
 	}
 	
 	void Update () {
-	
+		if (Time.time - lastTime >= frequency) {
+			string start = "";
+			switch(Random.Range(0,2)) {
+			case(0):
+				start = "Node";
+				break;
+			case(1):
+				start = "Node (4)";				
+				break;
+			case(2):
+				start = "Node (9)";			
+				break;
+			}
+			GameObject mob = Instantiate (_mob.transform.gameObject);
+			mob.transform.gameObject.GetComponent<MobController> ().startingNode = GameObject.Find (start).GetComponent<Node>();
+			mob.transform.position = GameObject.Find (start).transform.position + (Vector3.up * 0.5f);
+			lastTime = Time.time;
+		}
 	}
 }
