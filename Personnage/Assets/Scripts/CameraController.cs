b836a9343ed6531083605f6d7c282d5b2f6ea6a8
Gabriel Beaudry <gabriel.bdry@gmail.com>
2016-01-23 01:04:58 -0500

    Update Personnage

---
+++ Personnage/Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public PlayerController player;
+    public float distance = 10.0f;
+    public float sensitivity = 3.0f;
+
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start () {
+        offset = (transform.position - player.transform.position).normalized * distance;
+        transform.position = player.transform.position + offset;
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        player.UpdateDirection(transform.forward);
+        Quaternion qX = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * sensitivity, Vector3.up);
+        Quaternion qY = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * sensitivity, Vector3.Cross(new Vector3(transform.forward.x, 0.0f, transform.forward.z), Vector3.up));
+        offset = qX * qY * offset;
+        transform.rotation = qX * qY * transform.rotation;
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Personnage/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+    private Vector3 moveFoward;
+
+    // States...
+    bool isCrouching;
+    bool is_Standing;
+    bool is_Walking;
+    bool is_Running;
+    bool is_Holding;
+    bool is_Jumping;
+    bool is_Reloading;
+
+    // Use this for initialization
+    void Start ()
+    {
+        is_Standing = true;
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezePositionY;
+	}
+	
+    public void UpdateDirection(Vector3 Foward)
+    {
+        moveFoward = Foward;
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        bool moveFront = Input.GetKey(KeyCode.W);
+        bool moveLeft = Input.GetKey(KeyCode.A);
+        bool moveRight = Input.GetKey(KeyCode.D);
+        bool moveBack = Input.GetKey(KeyCode.S);
+        is_Running = Input.GetKey(KeyCode.LeftShift);
+
+        Vector3 movement = Vector3.zero;
+
+        if (moveFront)
+        {
+            movement += moveFoward.normalized;
+        }
+        if (moveBack)
+        {
+            movement -= moveFoward.normalized;
+        }
+        if (moveLeft)
+        {
+            movement += Vector3.Cross(moveFoward, Vector3.up).normalized;
+        }
+        if (moveRight)
+        {
+            movement -= Vector3.Cross(moveFoward, Vector3.up).normalized;
+        }
+
+        rb.velocity = movement * speed * ((is_Running) ? 2 : 1);
+	}
+}
