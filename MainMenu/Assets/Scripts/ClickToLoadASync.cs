b38faf2b5c929cd0b6e6999c3550ffa82f297f7d
Gabriel Beaudry <gabriel.bdry@gmail.com>
2016-01-23 14:36:43 -0500

    MainMenu

---
+++ MainMenu/Assets/Scripts/ClickToLoadASync.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ClickToLoadASync : MonoBehaviour {
+
+    public Slider LoadingBar;
+    public GameObject LoadingImage;
+
+    private AsyncOperation async;
+
+    public void ClickASync(int level)
+    {
+        LoadingImage.SetActive(true);
+        StartCoroutine(LoadLevelWithBar(level));
+    }
+
+    IEnumerator LoadLevelWithBar(int level)
+    {
+        async = Application.LoadLevelAsync(level);
+        while (!async.isDone)
+        {
+            LoadingBar.value = async.progress;
+            yield return null;
+        }
+    }
+
+
+}
---
+++ MainMenu/Assets/Scripts/Quit.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quit : MonoBehaviour {
+
+	public void ClickToQuit()
+    {
+        Application.Quit();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Escape))
+            Application.Quit();
+	}
+}
