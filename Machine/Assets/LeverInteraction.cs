3ab39f8d0297f9fde46b0c8574516c67b26f1241
Nicolas Coutu <nico.coutu@hotmail.com>
2016-01-23 01:46:20 -0500

    Ajout du script des leviers

--- Machine/Assets/LeverInteraction.cs
+++ Machine/Assets/LeverInteraction.cs
@@ -3,16 +3,67 @@ using System.Collections;
 
 public class LeverInteraction : MonoBehaviour {
 
-	delegate void Use();
+	public float breakingRatio;
+	static private bool[] pattern;
+	static private bool[] state;
+
+	void Start () {
+		pattern = new bool[6];
+		state = new bool[6];
+		for (int i = 0; i < 6; i++) {
+			pattern [i] = (Random.Range(0,1) == 1);
+			state [i] = pattern [i]; 
+		}
+	}
 
 	void Update () {
-		if(Input.GetKeyDown (KeyCode.Q))
-			Creator.Break ();
-		if (Input.GetKeyDown (KeyCode.W))
-			Creator.Repair ();
-		if (Input.GetKeyDown (KeyCode.E))
-			Collector.Break ();
-		if (Input.GetKeyDown (KeyCode.R))
-			Collector.Repair ();
+		if (Random.value > breakingRatio) {
+			uint untouched = (uint)Random.Range (0,2);
+			if (Random.value >= 0.5f) {
+				for (int i = 0; i < 3; i++) {
+					if (i != untouched)
+						pattern [i] = !pattern [i];
+				}
+			} 
+			else {
+				untouched += 3;
+				for (int i = 3; i < 6; i++) {
+					if (i != untouched)
+						pattern [i] = !pattern [i];
+				}
+			}
+
+		}
+		for (int i = 0; i < 6; i++) {
+			if (pattern [i] != state [i]) {
+				if (i < 3)
+					Creator.Break ();
+				else
+					Collector.Break ();
+			}
+		}
+	}
+
+	static public void Use(GameObject lever){
+		switch (lever.tag) {
+		case("L1"):
+			state [0] = !state [0];
+			break;
+		case("L2"):
+			state [1] = !state [1];
+			break;
+		case("L3"):
+			state [2] = !state [2];
+			break;
+		case("L4"):
+			state [3] = !state [3];
+			break;
+		case("L5"):
+			state [4] = !state [4];
+			break;
+		case("L6"):
+			state [5] = !state [5];
+			break;
+		}
 	}
 }
