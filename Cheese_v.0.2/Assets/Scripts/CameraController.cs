b9120d2f8557c0d1eefd4b5d4202526d42769fac
SirKhepre <brandonroberge@hotmail.ca>
2016-01-24 03:14:01 -0500

    Camera

--- Cheese_v.0.2/Assets/Scripts/CameraController.cs
+++ Cheese_v.0.2/Assets/Scripts/CameraController.cs
@@ -3,32 +3,54 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
-    public PlayerController player;
-    public float crouchingHeight = 0.75f;
-    public float distance = 10.0f;
-    public float sensitivity = 3.0f;
-    public float boxCheckDistance = 6.0f;
+        public PlayerController player;
+        public float crouchingHeight = 0.75f;
+        public float distance = 5.0f;
+        public float minDistance = 0f;
+        public float maxDistance = 3.0f;
+        public float sensitivity = 3.0f;
+        public float boxCheckDistance = 6.0f;
 
-    private Vector3 offset;
+        private Vector3 offset;
+    private Vector3 rotTangent;
+    private bool collided = false;
 
-    // Use this for initialization
-    void Start () {
-        offset = (transform.position - player.transform.position).normalized * distance;
-        transform.position = player.transform.position + offset;
-    }
-	
-	// Update is called once per frame
-	void LateUpdate () {
+        // Use this for initialization
+        void Start() {
+            offset = (transform.position - player.transform.position).normalized;
+            transform.position = player.transform.position + offset * distance;
+        }
+
+    // Update is called once per frame
+    void Update() {
 
         player.UpdateDirection(transform.forward);
-        Quaternion qX = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * sensitivity, Vector3.up);
+        Quaternion qX = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * sensitivity, new Vector3(0, 0.9f, 0));
         Quaternion qY = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * sensitivity, Vector3.Cross(new Vector3(transform.forward.x, 0.0f, transform.forward.z), Vector3.up));
         offset = qX * qY * offset;
+        offset.Normalize();
         transform.rotation = qX * qY * transform.rotation;
+        if (transform.rotation.y < -0.6f)
+            transform.rotation.Set(transform.rotation.x, -0.6f, transform.rotation.z, transform.rotation.w);
+        else if (transform.rotation.y > 0.6f)
+            transform.rotation.Set(transform.rotation.x, 0.6f, transform.rotation.z, transform.rotation.w);
+        
+        Vector3 expectedCamPos = player.transform.position + offset * maxDistance;
 
-        if (!player.isCrouching())
-            transform.position = player.transform.position + offset;
+        RaycastHit hit = new RaycastHit();
+        if (Physics.Linecast(player.transform.position, expectedCamPos, out hit)) {
+            if (!hit.collider.gameObject.CompareTag("MainCamera") && !hit.collider.gameObject.CompareTag("Player")) {
+                expectedCamPos = hit.point;
+                if (collided) {
+                    expectedCamPos.y = transform.position.y;
+                }
+                collided = true;
+            }
+        } 
         else
-            transform.position = player.transform.position + offset + new Vector3(0, -crouchingHeight, 0);
+            collided = false;
+
+        transform.position = expectedCamPos;
     }
 }
+
