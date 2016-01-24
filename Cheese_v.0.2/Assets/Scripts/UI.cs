ea1acdb4950f7c54b0b89b849a804cb307c1e4f5
SirKhepre <brandonroberge@hotmail.ca>
2016-01-24 16:48:20 -0500

    Porte et autre

--- Cheese_v.0.2/Assets/Scripts/UI.cs
+++ Cheese_v.0.2/Assets/Scripts/UI.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using UnityEngine.UI; 
 
 public class UI : MonoBehaviour {
+    public BoxCollider box;
+    public GameObject dm;
 	public Text timetext;
 	private int min;
 	private int seconds;
@@ -28,32 +30,32 @@ public class UI : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		time += Time.deltaTime;
-		if (time >= 1) {
-			if(seconds != 0)
-				seconds -= 1;
-			else {
-				seconds = 59;
-				min -= 1;
-				
-			}
-			
-			time -= 1;
-		}
+        if (!dm.GetComponent<DoorManager>().CheckMoving()) {
+            time += Time.deltaTime;
+            if (time >= 1) {
+                if (seconds != 0)
+                    seconds -= 1;
+                else {
+                    seconds = 59;
+                    min -= 1;
 
-        if (min == 0 && seconds == 0)
-        {
-            min = 2;
-            GrabAndDrop.cheesestatus = 0;
+                }
 
+                time -= 1;
+            }
+
+            if (min == 0 && seconds == 0) {
+                min = 2;
+                GrabAndDrop.cheesestatus = 0;
+                dm.GetComponent<DoorManager>().Toggle();
+            }
+            timetext.transform.position.Set(32, 133, 0);
+            timetext.text = string.Format("Next truck: {0:00}:{1:00}", min, seconds);
+            //timetext.text = "Next Truck :" + min.ToString () + ":" + seconds.ToString ();
+            objectivetruck.text = GrabAndDrop.cheesestatus.ToString() + "/" + GrabAndDrop.cheeseObj.ToString();
+            //moneyText.text = money.ToString();
+            upgrade.text = "Upgrade production :" + price.ToString() + "$";
         }
-		timetext.transform.position.Set (32, 133, 0);
-        timetext.text = string.Format("{0:00}:{1:00}", min, seconds); 
-		//timetext.text = "Next Truck :" + min.ToString () + ":" + seconds.ToString ();
-        objectivetruck.text = GrabAndDrop.cheesestatus.ToString() + "/" + GrabAndDrop.cheeseObj.ToString();
-		//moneyText.text = money.ToString();
-		upgrade.text = "Upgrade production :" + price.ToString() + "$";
-		
 	}
 	public void Upgrade(){
 		if(money >= price){
